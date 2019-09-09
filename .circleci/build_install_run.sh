c71ebdffed1f08157d8ac27e315a1ad9c7b45f05
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-09-08 23:36:38 -0700

    CircleCI python workflows

---
+++ .circleci/build_install_run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# for CircleCI job build_install_run
+
+set -e
+set -u
+set -o pipefail
+
+cd "$(dirname -- "${0}")/.."  # cd to project top-level
+
+set -x
+whoami
+pwd
+env | sort
+ls -la .
+uname -a
+python --version
+pip --version
+build-install.sh --uninstall
