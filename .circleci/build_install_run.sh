3b8e9f98540270e3454f41429c114a6b9afa5069
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-09-15 00:04:22 -0700

    CircleCI: `docker info` during script run
    NFC: code comments

--- .circleci/build_install_run.sh
+++ .circleci/build_install_run.sh
@@ -6,14 +6,21 @@ set -u
 set -o pipefail
 
 BUILD_INSTALL="$(dirname -- "${0}")/../tools/build-install.sh"
+
+# dump much information about the CircleCI environment
 set -x
 whoami
 pwd
 env | sort
 ls -la .
 uname -a
+docker info
 python --version
 pip --version
+
+# install ahead of time
 pip install --user twine
+
+# run the build-install.sh
 chmod +x "${BUILD_INSTALL}"  # force +x
 "${BUILD_INSTALL}" --uninstall
