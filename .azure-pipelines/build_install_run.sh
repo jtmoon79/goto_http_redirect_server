b0949fa0ddc788cbfcbd006c8c619d10df8fbc28
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-09-15 21:31:33 -0700

    Azure: add Azure-specific wrapper for build-install.sh

---
+++ .azure-pipelines/build_install_run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# for Azure Pipelines task
+
+set -e
+set -u
+set -o pipefail
+
+BUILD_INSTALL="$(dirname -- "${0}")/../tools/build-install.sh"
+
+# dump much information about the Azure Pipelines environment
+set -x
+whoami
+hostname
+pwd
+env | sort
+ls -la .
+uname -a
+docker info || true
+python --version
+pip --version
+
+# run the build-install.sh
+chmod +x "${BUILD_INSTALL}"  # force +x
+"${BUILD_INSTALL}" --uninstall
