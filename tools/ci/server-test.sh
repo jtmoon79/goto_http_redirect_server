73168da83c078acecbe603e4f7075a9608b9ef75
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-12-10 21:57:59 -0800

    server-test.sh: use localhost only

--- tools/ci/server-test.sh
+++ tools/ci/server-test.sh
@@ -16,13 +16,15 @@ set -u
 set -o pipefail
 
 readonly PROGRAM_NAME='goto_http_redirect_server'
+readonly IP_LISTEN='127.0.0.1'
 readonly PORT=55923  # hopefully not in-use!
-readonly URL="http://localhost:${PORT}"
+readonly URL="http://${IP_LISTEN}:${PORT}"
 
 set -x
 curl --version  # note curl, fail sooner if not installed
 "${PROGRAM_NAME}" --debug \
     --shutdown 8 \
+    --ip "${IP_LISTEN}" \
     --port ${PORT} \
     --from-to '/a' 'http://foo.com' \
     --status-path '/status' \
