4781b4b75b92ce92ee3c05f592b632a57bd6b46e
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-12-13 18:45:08 -0800

    CicleCI: builder script use shared tools, more concise
    Use shared tools under ./tools more often.
    
    Reduce `pip` installs to fewer commands.

--- .circleci/build-install-run.sh
+++ .circleci/build-install-run.sh
@@ -6,16 +6,16 @@
 # XXX: very similar to tools/build-install.sh
 #
 # TODO: break this up into jobs for Circle CI Pipeline
+# $PWD is presumed to be at project root directory
 
 set -e
 set -u
 set -o pipefail
 
-# $PWD is presumed to be at project root directory
 readonly PACKAGE_NAME='goto_http_redirect_server'
 readonly PROGRAM_NAME='goto_http_redirect_server'
-readonly MYPY_INI='./.config/mypy.ini'
 readonly REALPATH=./tools/realpath.sh
+readonly MYPY=./tools/mypy.sh
 
 # dump much information about the CircleCI environment
 set -x
@@ -31,20 +31,11 @@ pip --version
 pip list -vvv
 
 # install and upgrade necessary packages
-python -m pip install --quiet --user --upgrade pip
-python -m pip install --quiet --user --upgrade setuptools
-python -m pip install --quiet --user twine
-python -m pip install --quiet --user mypy
-python -m pip --version
-python -m twine --version
-python -m mypy --version
+python -m pip install --quiet --user --upgrade pip setuptools
+python -m pip install --quiet --user twine mypy
+python -m pip list --disable-pip-version-check --no-index -vvv
 
-# condensed from tools/build-install.sh
-# update path with potential pip install locations
-usersite=$(python -B -c 'import site; print(site.USER_SITE);')
-userbase=$(python -B -c 'import site; print(site.USER_BASE);')
-userbasebin=${userbase}/bin  # --user install location on Ubuntu
-export PATH="${PATH}:${usersite}:${userbase}:${userbasebin}"
+source ./tools/ci/PATH-add-pip-site.sh
 
 SERVER_TEST=$("${REALPATH}" "./tools/ci/server-test.sh")
 PY_TEST=$("${REALPATH}" "./goto_http_redirect_server/test/pytest.sh")
@@ -53,7 +44,8 @@ PY_TEST=$("${REALPATH}" "./goto_http_redirect_server/test/pytest.sh")
 python -m pip install --user --verbose -e '.[development]'
 
 # mypy test
-python -m mypy --config-file "${MYPY_INI}" 'goto_http_redirect_server/goto_http_redirect_server.py'
+"${MYPY}"
+
 # build
 version=$(python -B -c 'from goto_http_redirect_server import goto_http_redirect_server as gh;print(gh.__version__)')
 python setup.py -v bdist_wheel
