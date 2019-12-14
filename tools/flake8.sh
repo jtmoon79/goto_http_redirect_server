125bf8632b9ed426ebddca3ed4fb2f9d90cc9f4d
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-12-13 22:54:06 -0800

    tools: add flake8

---
+++ tools/flake8.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# run flake8 with necessary settings
+
+set -e
+set -u
+
+cd "$(dirname -- "${0}")/.."
+
+flake8 --version
+set -x
+exec \
+    flake8 \
+        --config=./.config/flake8.ini \
+        goto_http_redirect_server/goto_http_redirect_server.py \
+        setup.py
