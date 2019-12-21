78a47a6e02e743eeba4530085d525633313f5094
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2019-12-20 23:19:50 -0800

    tools: Add service-uninstall.sh, align service-install.sh
    Add tools/ci/service-uninstall.sh
    
    Improve service-install.sh to be similar to service-uninstall.sh
    - use getopt
    - imrove debug output

--- tools/ci/service-install.sh
+++ tools/ci/service-install.sh
@@ -4,14 +4,11 @@
 # optionally, enable the service
 #
 # assumes goto_http_redirect_server has been installed and is in the $PATH
-#
-# usage:
-#      service-install.sh [--enable] 
+ 
 
 set -e
 set -u
-
-cd "$(dirname -- "${0}")/../.."
+set -o pipefail
 
 GOTO_FILE_REDIRECTS=/usr/local/share/goto_http_redirect_server.csv
 GOTO_FILE_SCRIPT=/usr/local/bin/goto_http_redirect_server
@@ -20,13 +17,64 @@ GOTO_CONFIG=/etc/goto_http_redirect_server.conf
 GOTO_SERVICE=goto_http_redirect_server.service
 GOTO_FILE_SERVICE=/etc/systemd/user/${GOTO_SERVICE}
 
+enable=false
+start=false
+
+if ! which getopt &>/dev/null; then
+    echo "ERROR: GNU getopt not found. It is part of util-linux package." >&2
+    exit 1
+fi
+
+options=$(getopt -n "$(basename -- "${0}")" -o "esh?" -l "enable,start,help" -- "${@}")
+eval set -- "${options}"
+
+while true; do
+    case "${1-}" in
+        -e|--enable)
+            enable=true
+            shift
+            ;;
+        -s|--start)
+            start=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            ;&
+        h|help)
+            ;&
+        \?)
+            (
+                echo "Usage: ${0} [-e|--enable] [-s|--start]"
+                echo
+                echo "       -e  enable systemd service"
+                echo "       -s  start systemd service (requires -e)"
+                echo
+                echo "Usage: ${0} [-h|--help|-?]"
+                echo
+                echo "       This help message"
+            ) >&2
+            exit 4
+            ;;
+    esac
+done
+
+if ${start} && ! ${enable}; then
+    echo "Warning: --start without --enable will probably fail" >&2
+fi
+
+cd "$(dirname -- "${0}")/../.."
+
 # create redirects file
 touch "${GOTO_FILE_REDIRECTS}"
 chmod -v 0644 -- "${GOTO_FILE_REDIRECTS}"
 
-# link goto_http_redirect_server
+# cp or link goto_http_redirect_server
 if which goto_http_redirect_server &>/dev/null; then
-    ln -vs -- "$(which goto_http_redirect_server)" "${GOTO_FILE_SCRIPT}"
+    ln -fvs -- "$(which goto_http_redirect_server)" "${GOTO_FILE_SCRIPT}"
 else
     cp -v -- ./goto_http_redirect_server/goto_http_redirect_server.py "${GOTO_FILE_SCRIPT}"
 fi
@@ -39,10 +87,11 @@ chmod -v 0755 -- "${GOTO_SYSTEMD_SH}"
 cp -v -- ./service/goto_http_redirect_server.conf "${GOTO_CONFIG}"
 chmod -v 0600 -- "${GOTO_CONFIG}"
 
-# install systemd service
+# copy systemd service
 cp -v -- ./service/goto_http_redirect_server.service "$(dirname -- "${GOTO_FILE_SERVICE}")"
 chmod -v 0444 -- "${GOTO_FILE_SERVICE}"
 
+# note settings of important files
 ls -l \
     "${GOTO_FILE_REDIRECTS}" \
     "${GOTO_FILE_SCRIPT}" \
@@ -50,6 +99,16 @@ ls -l \
     "${GOTO_CONFIG}" \
     "${GOTO_FILE_SERVICE}"
 
-if [[ "${1:-}" == '--enable' ]]; then
-    systemctl enable "${GOTO_FILE_SERVICE}"
+if ${enable}; then
+    (
+        set -x
+        systemctl enable "${GOTO_FILE_SERVICE}"
+    )
+fi
+
+if ${start}; then
+    (
+        set -x
+        systemctl start "${GOTO_SERVICE}"
+    )
 fi
---
+++ tools/ci/service-uninstall.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+#
+# remove systemd service files
+# optionally, --reload systemd
+# optionally, --wipe goto_http_redirect_server configuration files
+#
+ 
+
+set -e
+set -u
+set -o pipefail
+
+GOTO_FILE_REDIRECTS=/usr/local/share/goto_http_redirect_server.csv
+GOTO_SYSTEMD_SH=/usr/local/bin/goto_http_redirect_server.sh
+GOTO_CONFIG=/etc/goto_http_redirect_server.conf
+GOTO_SERVICE=goto_http_redirect_server.service
+GOTO_FILE_SERVICE=/etc/systemd/user/${GOTO_SERVICE}
+
+reload=false
+wipe=false
+
+if ! which getopt &>/dev/null; then
+    echo "ERROR: GNU getopt not found. It is part of util-linux package." >&2
+    exit 1
+fi
+
+options=$(getopt -n "$(basename -- "${0}")" -o "rwh?" -l "reload,wipe,help" -- "${@}")
+eval set -- "${options}"
+
+while true; do
+    case "${1-}" in
+        -r|--reload)
+            reload=true
+            shift
+            ;;
+        -w|--wipe)
+            wipe=true
+            shift
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            ;&
+        h|help)
+            ;&
+        \?)
+            (
+                echo "Usage: ${0} [-r|--reload] [-w|--wipe]"
+                echo
+                echo "       -r  reload systemd"
+                echo "       -w  wipe (remove) configuration files:"
+                echo "           ${GOTO_CONFIG}"
+                echo "           ${GOTO_FILE_REDIRECTS}"
+                echo
+                echo "Usage: ${0} [-h|--help|-?]"
+                echo
+                echo "       This help message"
+            ) >&2
+            exit 4
+            ;;
+    esac
+done
+
+cd "$(dirname -- "${0}")/../.."
+
+set +e            # no `set -e`; attempt to remove as much as possible
+declare -i ret=0  # but signal remove failures in script return code
+
+rm -v -- "${GOTO_SYSTEMD_SH}" "${GOTO_FILE_SERVICE}" || ret=1
+
+if ${reload}; then
+    (
+        set -x
+        systemctl stop "${GOTO_SERVICE}"
+        systemctl disable "${GOTO_SERVICE}"
+    )
+    rm -v -- "${GOTO_SYSTEMD_SH}" "${GOTO_FILE_SERVICE}" || ret=1
+    (
+        set -x
+        systemctl daemon-reload
+        systemctl reset-failed
+    )
+    shift
+fi
+
+if ${wipe}; then
+    rm -v -- "${GOTO_CONFIG}" "${GOTO_FILE_REDIRECTS}" || ret=1
+    shift
+fi
+
+exit ${ret}
