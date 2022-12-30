c8f9c274dc8a10171e9039ec52824528d1ae4fc2
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-12-30 13:45:06 -0800

    circleci flake8 based on Python version

--- setup.py
+++ setup.py
@@ -59,7 +59,8 @@ PACKAGE_DATA = GOTO_SERVICE_FILES + [
 
 # Python version >3.6 ?
 PYVER_GT36 = sys.version_info.major >= 3 and sys.version_info.minor > 6
-
+# Python version >3.7 ?
+PYVER_GT37 = sys.version_info.major >= 3 and sys.version_info.minor > 7
 
 class GotoSetupCommand(Command, abc.ABC):
     """
@@ -210,7 +211,7 @@ setup(
         # install these locally with command:
         #     python -m pip install --user -e '.[development]'
         "development": [
-            "flake8==6.0.0" if PYVER_GT36 else "flake8==5.0.4",
+            "flake8==6.0.0" if PYVER_GT37 else "flake8==5.0.4",
             "mypy==0.991" if PYVER_GT36 else "mypy==0.971",
             "pytest==7.2.0" if PYVER_GT36 else "pytest==6.2.5",
             "pytest-cov==4.0.0",
@@ -219,7 +220,7 @@ setup(
         ],
         # subsets of 'development' for faster `pip install` in CI stages
         "development-flake8": [
-            "flake8==6.0.0" if PYVER_GT36 else "flake8==5.0.4",
+            "flake8==6.0.0" if PYVER_GT37 else "flake8==5.0.4",
         ],
         "development-mypy": [
             "mypy==0.991" if PYVER_GT36 else "mypy==0.971",
